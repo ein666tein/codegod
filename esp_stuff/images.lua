b9a709dc7777dd397f12032ee061ed47aa315ea0
John Salvatòre <ptickcount@gmail.com>
2019-01-31 17:12:12 +0300

    debugged errors

--- esp_stuff/images.lua
+++ esp_stuff/images.lua
@@ -1,17 +1,13 @@
-images_lib = {}
+images_lib = { }
 images_lib.__index = images_lib
 images_lib.textures = { }
 
 function images_lib:generate_textures()
     for name, data in pairs(_icons) do
-
-        if images_lib.textures[name] then
-            local img_rgba, width, height = common.RasterizeSVG(data[3])
-            draw.UpdateTexture(images_lib.textures[name][3], img_rgba)
-        else
-            local img_rgba, width, height = common.RasterizeSVG(data[3])
+		local img_rgba, width, height = common.RasterizeSVG(data[3])
+	
+        if images_lib.textures[name] then draw.UpdateTexture(images_lib.textures[name][3], img_rgba) else
             local texture = draw.CreateTexture(img_rgba, width, height)
-
             images_lib.textures[name] = { width / 2, height / 2, texture, img_rgba }
         end
     end
@@ -22,7 +18,6 @@ function images_lib:draw_texture(pos, color, data)
     x = x - data[1] / 2
 
     draw.SetTexture(data[3])
-
     draw.Color(color[1], color[2], color[3], color[4])
     draw.FilledRect(x, y, x + data[1], y + data[2])
 end
